3e2584dc060b6360f6f1cc7b5a4978df6d6f58da
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-02-26 23:39:36 -0500

    Lab 4 done

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -6,12 +6,23 @@ import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
 import Add from "./Add";
 import TodoList from "./todos/TodoList";
+import { LabState } from "../store";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
+
   return (
     <div className="container">
       <h1>Assignment 3</h1>
       <TodoList />
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       <Add a={3} b={4} />
       <Highlight>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
