21f4900d2c7e16ecf5b36566b36f9d5f3f33aae1
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-02-28 18:51:09 -0500

    Refactor using reducer (3.3.4)

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,41 +1,26 @@
 import React, { useState } from "react";
 import "./index.css";
-import db from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+import { KanbasState } from "../../store";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const [moduleList, setModuleList] = useState(db.modules);
-  const [module, setModule] = useState({
-    _id: "",
-    name: "New Module",
-    description: "New Description",
-    course: courseId,
-  });
-  const addModule = (module: any) => {
-    const newModule = { ...module, _id: new Date().getTime().toString() };
-    const newModuleList = [newModule, ...moduleList];
-    setModuleList(newModuleList);
-  };
-  const deleteModule = (moduleId: string) => {
-    const newModuleList = moduleList.filter(
-      (module) => module._id !== moduleId,
-    );
-    setModuleList(newModuleList);
-  };
-  const updateModule = () => {
-    const newModuleList = moduleList.map((m) => {
-      if (m._id === module._id) {
-        return module;
-      } else {
-        return m;
-      }
-    });
-    // @ts-ignore
-    setModuleList(newModuleList);
-  };
+  const moduleList = useSelector(
+    (state: KanbasState) => state.modulesReducer.modules,
+  );
+  const module = useSelector(
+    (state: KanbasState) => state.modulesReducer.module,
+  );
+  const dispatch = useDispatch();
 
   const [selectedModule, setSelectedModule] = useState(moduleList[0]);
   return (
@@ -54,30 +39,22 @@ function ModuleList() {
           {/*</button>*/}
           {/*Todo: change group style*/}
           <button
-            onClick={() => {
-              addModule(module);
-            }}
+            onClick={() => dispatch(addModule({ ...module, course: courseId }))}
           >
             Add
           </button>
-          <button onClick={updateModule}>Update</button>
+          <button onClick={() => dispatch(updateModule(module))}>Update</button>
 
           <input
             value={module.name}
             onChange={(e) =>
-              setModule({
-                ...module,
-                name: e.target.value,
-              })
+              dispatch(setModule({ ...module, name: e.target.value }))
             }
           />
           <textarea
             value={module.description}
             onChange={(e) =>
-              setModule({
-                ...module,
-                description: e.target.value,
-              })
+              dispatch(setModule({ ...module, description: e.target.value }))
             }
           />
         </div>
@@ -98,15 +75,14 @@ function ModuleList() {
                   <FaPlusCircle className="ms-2" />
                   <FaEllipsisV className="ms-2" />
                   {/*Todo: change deleted button style*/}
-                  <button className="me-2"
-                    onClick={(event) => {
-                      setModule(module);
-                    }}
+                  <button
+                    className="me-2"
+                    onClick={() => dispatch(setModule(module))}
                   >
                     Edit
                   </button>
 
-                  <button onClick={() => deleteModule(module._id)}>
+                  <button onClick={() => dispatch(deleteModule(module._id))}>
                     Delete
                   </button>
                 </span>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,6 +4,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Courses from "./Courses";
 import db from "./Database";
 import { useState } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Kanbas() {
   const [courses, setCourses] = useState(db.courses);
@@ -42,34 +44,36 @@ function Kanbas() {
   };
 
   return (
-    <div className="d-flex">
-      <div>
-        <KanbasNavigation />
+    <Provider store={store}>
+      <div className="d-flex">
+        <div>
+          <KanbasNavigation />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            {/*<Route path="/Account" element={<Account/>}/>*/}
+            <Route
+              path="/Dashboard"
+              element={
+                <Dashboard
+                  courses={courses}
+                  course={course}
+                  setCourse={setCourse}
+                  addNewCourse={addNewCourse}
+                  deleteCourse={deleteCourse}
+                  updateCourse={updateCourse}
+                />
+              }
+            />
+            <Route
+              path="Courses/:courseId/*"
+              element={<Courses courses={courses} />}
+            />
+          </Routes>
+        </div>
       </div>
-      <div style={{ flexGrow: 1 }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard" />} />
-          {/*<Route path="/Account" element={<Account/>}/>*/}
-          <Route
-            path="/Dashboard"
-            element={
-              <Dashboard
-                courses={courses}
-                course={course}
-                setCourse={setCourse}
-                addNewCourse={addNewCourse}
-                deleteCourse={deleteCourse}
-                updateCourse={updateCourse}
-              />
-            }
-          />
-          <Route
-            path="Courses/:courseId/*"
-            element={<Courses courses={courses} />}
-          />
-        </Routes>
-      </div>
-    </div>
+    </Provider>
   );
 }
 
