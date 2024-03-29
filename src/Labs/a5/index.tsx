a5eaedb6521d440356d18c351a63fa07939fe44a
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-03-28 20:52:07 -0400

    3.1 done

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+
 function Assignment5() {
   return (
     <div className="container">
       <h1>Assignment 5</h1>
       <a href="http://localhost:4000/a5/welcome">Welcome</a>
+      <EncodingParametersInURLs />
     </div>
   );
 }
