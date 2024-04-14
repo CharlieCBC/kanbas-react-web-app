a141cd88ce1cad8ce484786a7f4e1225d53e39ad
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-14 09:58:49 -0400

    update default values

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -19,15 +19,15 @@ function Dashboard({
         <h5>Course</h5>
         <input
           placeholder="Course Name"
-          value={course.name || "New Course"}
+          value={course.name}
           className="form-control mb-1"
-          onChange={(e) => setCourse({ ...course, name: e.target.value })}
+          onChange={(e) => setCourse({ ...course, name: e.target.value || "New Course"})}
         />
         <input
           placeholder="Course Number"
           value={course.number || "NC0001"}
           className="form-control mb-1"
-          onChange={(e) => setCourse({ ...course, number: e.target.value })}
+          onChange={(e) => setCourse({ ...course, number: e.target.value || "NC0001"})}
         />
         <input
           value={course.startDate || ""}
